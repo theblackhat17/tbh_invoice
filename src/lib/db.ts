336a24d1813fae93fb6f4baa6785e48d8a6dc112
Clément VANDEWALLE <clement.vandewalle@adeo.com>
2025-02-25 17:29:05 +0100

    update database and adapt to netlfy project

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,6 @@
 // src/lib/db.ts
 import Database from 'better-sqlite3';
-
 const db = new Database('invoices.db', { verbose: console.log });
-
 db.exec(`
   CREATE TABLE IF NOT EXISTS invoices (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +12,6 @@ db.exec(`
     userId TEXT,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP
   );
-  
   CREATE TABLE IF NOT EXISTS clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -26,5 +23,4 @@ db.exec(`
     created_at TEXT DEFAULT CURRENT_TIMESTAMP
   );
 `);
-
 export default db;
